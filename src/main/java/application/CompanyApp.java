1c78f80e8bf60edbef354e24b647bb6a0e95c87d
ratyj <jorma.raty@metropolia.fi>
2026-04-22 22:04:40 +0300

    Muokattu hieman alkuperäistä toimintaa.

--- src/main/java/application/CompanyApp.java
+++ src/main/java/application/CompanyApp.java
@@ -2,6 +2,9 @@ package application;
 
 import entity.*;
 import dao.*;
+
+import java.util.List;
+
 public class CompanyApp {
     public static void main(String[] args) {
 
@@ -11,14 +14,13 @@ public class CompanyApp {
         empdao.persist(new Employee("Viivi", "Puro", "viivip@mymail.fi", 7300.00));
         empdao.persist(new Employee("Tero", "Koski", "tero.koski@mymail.fi", 3750.00));
 
-        Employee emp = empdao.find(1);
-        System.out.println("-- Eka työntekijän nimi");
-        System.out.println(emp.getFirstName() + " " + emp.getLastName());
+        // haetaan kaikkien työntekijöiden tiedot
+        List<Employee> employees = empdao.findAll();
+        System.out.println("-- kaikkien työntekijöiden nimet ja palkka");
+        for (Employee e : employees) {
+            System.out.println(e.getFirstName() + " " + e.getLastName() + ", " + e.getSalary());
+        }
 
-        System.out.println("-- Muokataan eka työntekijän sukunimeä");
-        emp.setLastName("Koski");
-        empdao.update(emp);
-        System.out.println("-- Eka työntekijän tietoja:");
-        System.out.println(emp.getFirstName() + " " + emp.getLastName() + " email: " + emp.getEmail());
+
     }
 }
